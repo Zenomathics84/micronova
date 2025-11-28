69a0d7a2a3f04a1468a42542e3cb4e09d5f5d563
Zenomathics84 <vancoillie.pieter@telenet.be>
2025-11-28 16:15:37 +0100

    Use custom button to dump ROM/RAM memory values to debug log

--- components/micronova/button/micronova_button.cpp
+++ components/micronova/button/micronova_button.cpp
@@ -1,3 +1,4 @@
+
 #include "micronova_button.h"
 
 namespace esphome {
@@ -5,14 +6,17 @@ namespace micronova {
 
 void MicroNovaButton::press_action() {
   switch (this->get_function()) {
-    case MicroNovaFunctions::STOVE_FUNCTION_CUSTOM:
-    uint8_t locations[] = {0x00, 0x20};
-    for (uint8_t loc : locations) {
+    case MicroNovaFunctions::STOVE_FUNCTION_CUSTOM: {
+      uint8_t locations[] = {0x00, 0x20};
+      for (uint8_t loc : locations) {
         for (uint16_t addr = 0x00; addr <= 0xFF; ++addr) {
-            this->micronova_->queue_read_request(loc, static_cast<uint8_t>(addr));
+          this->micronova_->queue_read_request(loc, static_cast<uint8_t>(addr));
         }
-//      this->micronova_->queue_write_request(this->memory_location_, this->memory_address_, this->memory_data_);
+      }
+      // Optional: Uncomment if you need write requests
+      // this->micronova_->queue_write_request(this->memory_location_, this->memory_address_, this->memory_data_);
       break;
+    }
     default:
       break;
   }
@@ -21,3 +25,4 @@ void MicroNovaButton::press_action() {
 
 }  // namespace micronova
 }  // namespace esphome
+
