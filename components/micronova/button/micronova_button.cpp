56cfeba705441ca076c07367913eed0254e5a6c7
Zenomathics84 <vancoillie.pieter@telenet.be>
2025-11-28 18:13:18 +0100

    Update custom button to do queue_write_request again

--- components/micronova/button/micronova_button.cpp
+++ components/micronova/button/micronova_button.cpp
@@ -7,14 +7,15 @@ namespace micronova {
 void MicroNovaButton::press_action() {
   switch (this->get_function()) {
     case MicroNovaFunctions::STOVE_FUNCTION_CUSTOM: {
-      uint8_t locations[] = {0x00, 0x20};
-      for (uint8_t loc : locations) {
-        for (uint16_t addr = 0x00; addr <= 0xFF; ++addr) {
-          this->micronova_->queue_read_request(loc, static_cast<uint8_t>(addr));
-        }
-      }
-      // Optional: Uncomment if you need write requests
-      // this->micronova_->queue_write_request(this->memory_location_, this->memory_address_, this->memory_data_);
+      // memory dump to log
+      // uint8_t locations[] = {0x00, 0x20};
+      // for (uint8_t loc : locations) {
+      //   for (uint16_t addr = 0x00; addr <= 0xFF; ++addr) {
+      //     this->micronova_->queue_read_request(loc, static_cast<uint8_t>(addr));
+      //   }
+      // }
+      
+      this->micronova_->queue_write_request(this->memory_location_, this->memory_address_, this->memory_data_);
       break;
     }
     default:
